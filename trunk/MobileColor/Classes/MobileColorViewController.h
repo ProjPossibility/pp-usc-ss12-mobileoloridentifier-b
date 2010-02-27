fda18bda752a58d005c963d6e22581c871b5f1c9
mattkodiatte <mattkodiatte@28a3800e-23d3-11df-adc3-512f8641c811>
2010-02-27 20:27:41 +0000

    created a project

---
+++ trunk/MobileColor/Classes/MobileColorViewController.h
@@ -0,0 +1,16 @@
+//
+//  MobileColorViewController.h
+//  MobileColor
+//
+//  Created by Mathews Kodiatte on 2/27/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MobileColorViewController : UIViewController {
+
+}
+
+@end
+
---
+++ trunk/MobileColor/Classes/MobileColorViewController.m
@@ -0,0 +1,59 @@
+//
+//  MobileColorViewController.m
+//  MobileColor
+//
+//  Created by Mathews Kodiatte on 2/27/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import "MobileColorViewController.h"
+
+@implementation MobileColorViewController
+
+
+
+/*
+// The designated initializer. Override to perform setup that is required before the view is loaded.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
